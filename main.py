06c9d7fd1223c027e310d82c3a2d737fb7ef8b4d
blakewilfong <wilfong.blake@gmail.com>
2024-03-12 10:15:17 -0500

    clean up the f strings

--- main.py
+++ main.py
@@ -25,14 +25,13 @@ if choice == 1:
     english_phrase = list(input("Please enter the word or phrase you would like converted into morse code").upper())
     morse_code_phrase = [morse_code_dict[character] for character in english_phrase]
     translation = ", ".join(str(character) for character in morse_code_phrase)
-    print(f"Your phrase in morse code is {translation}")
+    print(f"Your phrase in Morse Code is {translation}")
 
 elif choice ==2:
     morse_code_phrase = input("Please enter the word or phrase in Morse Code that you would like to convert"
                               "to English.  Separate each character with a comma")
     morse_code_list = morse_code_phrase.split(",")
-    print(morse_code_list)
     morse_code_dict = {value: key for key, value in morse_code_dict.items()}
     english_phrase_list = [morse_code_dict[character] for character in morse_code_list]
     translation = "".join(str(character) for character in english_phrase_list)
-    print(translation)
+    print(f"Your phrase in English is {translation}")
